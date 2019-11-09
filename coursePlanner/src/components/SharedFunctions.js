1636bf81462277ee2f617f6f1d2c6a15bc7a79d0
Owen Mellema <owenmllm@gmail.com>
2019-11-09 10:43:37 -0600

    Refactor Code

---
+++ coursePlanner/src/components/SharedFunctions.js
@@ -0,0 +1,28 @@
+function getRemainingRequirements()
+{
+  //TODO: Make this do useful work.
+  return [1,2,3,4,5];
+}
+
+function getNameOfRequirement(requirement_id)
+{
+  //TODO: Return the name of the requirement.
+  return "Bruh Studies";
+}
+
+function getClasses(requirement_id)
+{
+  //TODO: Return an array of class ids that satisfy the requirements.
+  return ["448", "682"]
+}
+
+function getNameOfClass(class_id)
+{
+  //TODO: Return the name of the class
+  return "Ya Bruh or Nah Bruh: Modern Bruh Culture"
+}
+
+export {getRemainingRequirements};
+export {getNameOfRequirement};
+export {getClasses};
+export {getNameOfClass};
