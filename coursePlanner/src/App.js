2e1115ce3e5cf553fbffc817162e676fed3e7830
EllissaPeterson <ellissa@iastate.edu>
2019-11-09 02:13:29 -0600

    twilio number entry

--- coursePlanner/src/App.js
+++ coursePlanner/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import CHeader from './components/CustomHeader.js'
-import {Button} from 'reactstrap';
+import {Button, Input, Form} from 'reactstrap';
 import ModalExample from './components/ModalExample.js'
 import {Container, Row, Col} from 'react-bootstrap';
 
@@ -10,52 +10,287 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
-      name: "EP3PO",
-      programs: {}
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  sendText(name) {
-    // var Base64 = require('js-base64').Base64;
-    // let message = 'Hello ' + name;
-    // const account = 'AC8958e14b6120c8f3342812c4cdda594e';
-    // const token = 'e14286eeb3f4a4eb47c1c0ab5b7dd5ff';
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-    // let hash = Base64.encode( `${account}:${token}` );
+  sendText(phoneNumber) {
+    var Base64 = require('js-base64').Base64;
+    let message = 'Hello Owen';
+    const account = 'AC8958e14b6120c8f3342812c4cdda594e';
+    const token = 'e14286eeb3f4a4eb47c1c0ab5b7dd5ff';
 
-    // let form = new FormData();
-    // form.append( 'From', '+14154888651' );
-    // form.append( 'Body', message );
-    // form.append( 'To', '+15157201611' ); //send to post author
+    let hash = Base64.encode( `${account}:${token}` );
 
-    // fetch( `https://api.twilio.com/2010-04-01/Accounts/${account}/Messages.json`, {
-    //     method: 'POST',
-    //     headers: {
-    //     'Authorization': `Basic ${hash}`
-    //     }, 
-    //     body: form
-    // } )
-    // .then( ( response ) => response.json() )
-    // .then( ( data ) => {
-    //     console.log( data );
-    // } );
+    let form = new FormData();
+    form.append( 'From', '+14154888651' );
+    form.append( 'Body', message );
+    form.append( 'To', phoneNumber ); //send to post author
+    console.log(phoneNumber);
+
+    fetch( `https://api.twilio.com/2010-04-01/Accounts/${account}/Messages.json`, {
+        method: 'POST',
+        headers: {
+        'Authorization': `Basic ${hash}`
+        }, 
+        body: form
+    } )
+    .then( ( response ) => response.json() )
+    .then( ( data ) => {
+        console.log( data );
+    } );
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.sendText(this.state.phoneNumber);
   }
 
   render () {
+    const {
+      phoneNumber
+    } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <CHeader/>
+          <br/>
+          <br/>
           <ModalExample buttonLabel="Modal"/>
           <br/>
-          <Button color="info" onClick={this.sendText(this.state.name)} size="lg">Send Text</Button>
           <Container>
+          <Row>
+              <Col>
+                Semester
+              </Col>
+              <Col>
+                course 1
+              </Col>
+              <Col>
+                course 2
+              </Col>
+              <Col>
+                course 3
+              </Col>
+              <Col>
+                course 4
+              </Col>
+              <Col>
+                course 5
+              </Col>
+            </Row>
             <Row>
               <Col>
-
+                Semester 1
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Semester 2
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Semester 3
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Semester 4
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Semester 5
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Semester 6
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Semester 7
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+            </Row>
+            <Row>
+            <Col>
+                Semester 8
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
+              </Col>
+              <Col>
+                course number
+                credits
               </Col>
             </Row>
           </Container>
+          <Form onSubmit={this.handleSubmit}>
+            <Input type="text" name="phoneNumber" id="phoneNumber" value={phoneNumber} onChange={this.handleChange} placeholder="+12345678901"/>
+            <Button color="info" type="submit" size="lg">Send Schedule to: </Button>
+          </Form>
           <br/>
           <br/>
         </header>
